b6c28733f19d59878dc9b9ed366af8e1392a0f5d
imgradeone - 一年级么么哒 <imgradeone@outlook.com>
2021-06-12 15:40:14 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: '桂林中学 时光驿客',
+    description: '致力于制作更高品质视频的高中社团',
+  
+    head: [
+        ['meta', { name: 'theme-color', content: '#f57005' }],
+        ['link', { rel: 'icon', href: '/logo.png' }],
+        ['meta', { name: 'keywords', content: '桂林中学,桂林中学社团,摄影,摄影社团,高中社团,视频剪辑,社团,时光驿客' }]
+      ],
+  
+    themeConfig: {
+      logo: '/logo.png',
+      navbar: [
+        { text: '主页', link: '/' },
+        { text: '作品', link: '/works' },
+        { text: '关于', link: '/about' },
+      ],
+      notFound: ['抱歉，我们好像走远了......', '也许 imgradeone 这个鸽子又在咕咕咕了......'],
+      backToHome: '返回主页',
+    },
+  }
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+heroImage: /logo.png
+footer: © 2021 桂林中学 时光驿客 | 由 <a href="https://imgradeone.com" target="_blank">imgradeone</a> 维护 | 托管于 <a href="https://vercel.com" target="_blank">▲ Vercel</a> | <a href="http://www.glzx.net" target="_blank">桂林中学官网</a>
+footerHtml: true
+---
